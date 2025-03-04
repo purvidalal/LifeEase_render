391fa82a0b0490511a8aeddd60d1f5ca075bc66b
purvi <purvi@purvis-MacBook-Pro.local>
2025-03-04 22:34:20 +1100

    Modified personal_info

--- personal_info.py
+++ personal_info.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def load_personal_info():
     # Load the dataset
-    personal_info_data = pd.read_excel(r"/Users/purvi/Desktop/boaient/OpenAI model - lifeCare Pilot/lifeEase - Personal Input Sheet.xlsx")
+    personal_info_data = pd.read_excel("lifeEase - Personal Input Sheet.xlsx")
     
     # Group by category and join values
     personal_info_data_grouped = personal_info_data.groupby("Category").agg(lambda x: ' | '.join(x.dropna().astype(str)))
