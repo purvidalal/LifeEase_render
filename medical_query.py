c915ee73a6978ef79d19403db3dc96f8f275014d
Purvi Dalal <150809909+purvidalal@users.noreply.github.com>
2026-01-22 16:38:13 +1100

    Update medical_query.py

--- medical_query.py
+++ medical_query.py
@@ -12,9 +12,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 # TIR API Setup
 # ----------------------------------------------------------------------
 # You can edit your token, URL, and model here
-TIR_API_KEY = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJGSjg2R2NGM2pUYk5MT2NvNE52WmtVQ0lVbWZZQ3FvcXRPUWVNZmJoTmxFIn0.eyJleHAiOjE3NzY0MTQ0MDYsImlhdCI6MTc0NDg3ODQwNiwianRpIjoiMjA3NjAyMTctNmY3NC00NWJmLTliMDMtN2UzMTNmZTRhMDU4IiwiaXNzIjoiaHR0cDovL2dhdGV3YXkuZTJlbmV0d29ya3MuY29tL2F1dGgvcmVhbG1zL2FwaW1hbiIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJmZTk1YTdlMy1mYTIzLTRmYjgtYjkzMy00NWUwNGVkMGRjYjAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhcGltYW51aSIsInNlc3Npb25fc3RhdGUiOiI4OTIzZmYyNS1kMmRhLTQ3N2ItODI2YS0xNjVhZGJmZDMzMDYiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbIiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImFwaXVzZXIiLCJkZWZhdWx0LXJvbGVzLWFwaW1hbiJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsInNpZCI6Ijg5MjNmZjI1LWQyZGEtNDc3Yi04MjZhLTE2NWFkYmZkMzMwNiIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IlB1cnZpIERhbGFsIiwicHJpbWFyeV9lbWFpbCI6InN3YW1pQGJvYWllbnQuY29tIiwiaXNfcHJpbWFyeV9jb250YWN0IjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoicHVydmkuZGFsYWxAYm9haWVudC5jb20iLCJnaXZlbl9uYW1lIjoiUHVydmkiLCJmYW1pbHlfbmFtZSI6IkRhbGFsIiwiZW1haWwiOiJwdXJ2aS5kYWxhbEBib2FpZW50LmNvbSIsImlzX2luZGlhYWlfdXNlciI6ZmFsc2V9.kVFGHjoq3LLo-mvEar25jejsrWZHa9RDt9FA9LvuIiIT6DpRjPrfdQh2CyuvXa3fGaelnDU3xYUiDpEjLZQb7CpWdXSNs2UF6pp26l-CApHiG8ty-ksYpYOtal_jp5tWrQNZg-aGNdqjRAhLmqOk5wjlEuyGMX0P4D9UfCZedVI"
-TIR_BASE_URL = "https://infer.e2enetworks.net/project/p-5263/endpoint/is-4799/v1/"
-TIR_MODEL_NAME = "peft-model"
+TIR_API_KEY = 
+TIR_BASE_URL = 
+TIR_MODEL_NAME = 
 
 # Connect to TIR
 client = OpenAI(
